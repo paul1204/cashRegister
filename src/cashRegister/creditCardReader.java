dfbe0cea0bb1097cec8449dc4235f503e21dd5d9
Paramveer Singh <singhp35@mymail.vcu.edu>
2021-08-02 19:18:51 -0400

    Updated Comments

---
+++ src/cashRegister/creditCardReader.java
@@ -0,0 +1,29 @@
+package cashRegister;
+
+public class creditCardReader {
+	//This Card Reader is Located within the Store
+	
+	
+	//We instantiate a Credit Card here because we will be using it on the Card Reader.
+	creditCardMock card = new creditCardMock(13141516 ,"Bugs Bunny", 7536);
+	private int samplePin = 7536;
+	
+	public creditCardReader() {
+		initialize();
+	}
+	
+	public creditCardMock acceptCard(){
+		return card;
+	}
+	
+	//Customer Typing in their Pin
+	public int acceptPin() {
+		//Implement a Pin Pad UI
+		return samplePin;
+	}
+	
+	private void initialize() {
+		//Turn on 
+	}
+	
+}
