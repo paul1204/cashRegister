35f5f434304613f878776e3cb2aa5ccb2425e717
Paramveer Singh <singhp35@mymail.vcu.edu>
2021-08-02 20:20:11 -0400

    gasPumps Inherits Card Authorization

--- src/cashRegister/gasPumps.java
+++ src/cashRegister/gasPumps.java
@@ -1,43 +1,24 @@
 package cashRegister;
 
-public class gasPumps extends pump{
+public class gasPumps extends cardAuth{
 	creditCardMock card = new creditCardMock(13141516 ,"Bugs Bunny", 7536);
 	
 	int pumpNum;
 	int selectOctane = -1;
-	int regular = 0;
-	int midGrade = 1;
-	int premium = 2;
+	final int regular = 0;
+	final int midGrade = 1;
+	final int premium = 2;
 	
 
-	@Override
-	void pumpFuel() {
-		// TODO Auto-generated method stub
-		
-	}
- 
-	@Override
-	creditCardMock readCard() {
-		return card;
-		
-	}
-
-	@Override
-	void selectOctance() {
-		// TODO Auto-generated method stub
-		
+	public boolean auth(creditCardMock card, int zip, double price) {
+		return cardProcess.bankAuth(card, zip, price);
 	}
 	
 }
 
-enum Octane{Regular,Midgrade,Premium}
 
-abstract class pump{	
-	abstract void selectOctance();
-	void selectOctane(Octane gas) {}
-	abstract void pumpFuel(); 
-	abstract creditCardMock readCard();
-}
+
+
 
 
 
